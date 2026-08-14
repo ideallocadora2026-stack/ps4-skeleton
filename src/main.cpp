48894c81df0609bf8b72695fbefbf27d7c0ff2cb
ideallocadora2026-stack <ideallocadora2026@gmail.com>
2026-08-14 11:02:41 -0300

    Refactor main function to load webkit application
    Replaced the previous main function with a new implementation that loads a webkit application and enters an infinite loop.

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,15 @@
-// This is an open source non-commercial project. Dear PVS-Studio, please check it.
-// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
-
-// License: 0BSD
-
-#include "utils.h"
-
-#include "libLog.h"
-#include "notifi.h"
-
-#include <orbis/SystemService.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <orbis/libkernel.h>
+#include <orbis/SystemService.h>
 
-#include <stddef.h>
-
-// This function will display p_Message, then "cleanly" close the application
-void fail(const char *p_Message) {
-  logKernel(LL_Fatal, "%s", p_Message);
-  notifi(NULL, "%s", p_Message);
-  sceSystemServiceLoadExec("exit", NULL);
-}
-
-int main() {
-  // Jailbreak
-  jailbreak();
-
-  // Pop notification depending on jailbreak result
-  if (is_jailbroken()) {
-    notifi(NULL, "%s", "Jailbreak succeeded!");
-  } else {
-    notifi(NULL, "%s", "Jailbreak failed!");
-  }
-
-  // Back to jail
-  unjailbreak();
-
-  // Sleep for 7 seconds so the system has time to display the notification
-  sceKernelSleep(7);
-
-  // "Clean" close
-  sceSystemServiceLoadExec("exit", NULL);
-
-  // We should never get here
-  for (;;) {
-  }
-
-  return 0;
+int main(void) {
+    char *args[] = {
+        "pstorage:///data/self/Boyceta-ps4/index.html",
+        NULL
+    };
+    sceSystemServiceLoadExec("com.playstation.webkit", args);
+    while(1) { sceKernelSleep(1); }
+    return 0;
 }
