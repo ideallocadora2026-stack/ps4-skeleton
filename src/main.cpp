a4541cee52bfff1803d1ec0069e30a40d413e981
ideallocadora2026-stack <ideallocadora2026@gmail.com>
2026-08-15 12:12:10 -0300

    Refactor main function to hide splash screen
    Replaced the previous main function with a new implementation that hides the splash screen and enters an infinite loop.

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <orbis/libkernel.h>
 #include <orbis/SystemService.h>
 
-int main(void) {
-    const char *args[] = {
-        "com.playstation.webkit",
-        "file:///app0/index.html",
-        NULL
-    };
-    sceSystemServiceLoadExec("com.playstation.webkit", args);
-    while(1) { sceKernelSleep(1); }
+int main(void)
+{
+    sceSystemServiceHideSplashScreen();
+
+    while (1)
+    {
+        sceKernelSleep(1);
+    }
+
     return 0;
 }
